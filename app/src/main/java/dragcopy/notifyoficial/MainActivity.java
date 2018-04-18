3d99faa97744f53af9870b3f5daefd664e013b61
Richard095 <ricardorm789@gmail.com>
2018-04-17 23:58:42 -0500

    Prueba 1 Y estamos listos

--- app/src/main/java/dragcopy/notifyoficial/MainActivity.java
+++ app/src/main/java/dragcopy/notifyoficial/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //Ejemplo para verificar cambios en repositorio
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
