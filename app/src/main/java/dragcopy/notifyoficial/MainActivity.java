e83d490a006d25ba885013c29a85d187b7f57596
Richard095 <ricardorm789@gmail.com>
2018-04-18 00:06:31 -0500

    Prueba dos

--- app/src/main/java/dragcopy/notifyoficial/MainActivity.java
+++ app/src/main/java/dragcopy/notifyoficial/MainActivity.java
@@ -2,14 +2,17 @@ package dragcopy.notifyoficial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    //Ejemplo para verificar cambios en repositorio
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button Into  = (Button) findViewById(R.id.button_start);
     }
 }
