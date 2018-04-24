7715cd061d34bbb525d3758c8cc812268c8a98cc
CarlosAEH <carlosescalante28@gmail.com>
2018-04-23 23:25:35 -0500

    StartActivity

---
+++ app/src/main/java/dragcopy/notifyoficial/StartActivity.java
@@ -0,0 +1,29 @@
+package dragcopy.notifyoficial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        new CountDownTimer(2000,1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent i = new Intent(StartActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }.start();
+    }
+}
