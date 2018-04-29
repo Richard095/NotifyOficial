e9d664cfe509b94dd84465a67910f0fefc5a384c
CarlosAEH <carlosescalante28@gmail.com>
2018-04-29 13:58:04 -0500

    ActionBar,mas cardviews y otros toques visuales

--- app/src/main/java/dragcopy/notifyoficial/InstitutoActivity.java
+++ app/src/main/java/dragcopy/notifyoficial/InstitutoActivity.java
@@ -9,8 +9,6 @@ import android.view.View;
 
 public class InstitutoActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
